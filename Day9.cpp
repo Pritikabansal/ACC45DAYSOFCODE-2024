ad2d0e91c5ee25515bb7881dd62b0671d523c550
Pritikabansal <pritikabansal0013@gmail.com>
2024-10-06 20:07:44 +0530

    added day9 code

---
+++ Day9.cpp
@@ -0,0 +1,33 @@
+/*
+Day - 9
+code - Determine the Score
+
+Chef appeared for a placement test.
+There is a problem worth XX points. Chef finds out that the problem has exactly 1010 test cases. It is known that each test case is worth the same number of points.
+Chef passes NN test cases among them. Determine the score Chef will get.
+
+NOTE: See sample explanation for more clarity.
+
+Input Format - 
+First line will contain TT, number of test cases. Then the test cases follow.
+Each test case contains of a single line of input, two integers XX and NN, the total points for the problem and the number of test cases which pass for Chef's solution.
+
+Output Format - 
+For each test case, output the points scored by Chef.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int x,n;
+	    cin>>x>>n;
+	    cout<<(x/10)*n<<endl;
+	    
+	}
+}
