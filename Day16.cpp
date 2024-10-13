b304d011a6d67bc387609fb8da8b4dde8855f309
Pritikabansal <pritikabansal0013@gmail.com>
2024-10-13 17:44:53 +0530

    added day16 code

---
+++ Day16.cpp
@@ -0,0 +1,31 @@
+/*
+Day - 16
+code - Count the Notebooks
+
+You know that 11 kg of pulp can be used to make 10001000 pages and 11 notebook consists of 100100 pages.
+Suppose a notebook factory receives NN kg of pulp, how many notebooks can be made from that?
+
+Input Format - 
+First line will contain TT, the number of test cases. Then the test cases follow.
+Each test case contains a single integer NN - the weight of the pulp the factory has (in kgs).
+
+Output Format - 
+For each test case, output the number of notebooks that can be made using NN kgs of pulp.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    cout<<n*10<<endl;
+	}
+}
+
+
