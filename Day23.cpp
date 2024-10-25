6652548231ce70d4fd1a4fb40eda539c717133b9
Pritikabansal <pritikabansal0013@gmail.com>
2024-10-25 10:30:04 +0530

    added day23 code2

--- Day23.cpp
+++ Day23.cpp
@@ -36,3 +36,37 @@ int main() {
 	    }
 	}
 }
+
+/*
+code2 - Minimum Pizzas
+
+Each pizza consists of 4 slices. There are N friends and each friend needs exactly X slices.
+
+Find the minimum number of pizzas they should order to satisfy their appetite.
+
+Input Format -
+The first line of input will contain a single integer T, denoting the number of test cases.Each test case consists of two integers N and X, the number of friends and the number of slices each friend wants respectively.
+
+Output Format -
+For each test case, output the minimum number of pizzas required.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    int total_slices = n*x;
+	    if(total_slices%4>0){
+	        cout<<total_slices/4 +1<<endl;
+	    }
+	    else{
+	        cout<<total_slices/4<<endl;
+	    }
+	}
+}
